19109ab1c719dfb1167d233ce11fdf9b1832ca22
Amanjot Singh <98868918+Amanjot-001@users.noreply.github.com>
2023-05-15 12:18:13 +0530

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,10 @@
+// window.onload = function () {
+//     const nav = document.querySelector('nav');
+//     const timer = document.querySelector('.timer');
+//     const result = document.querySelector('.result');
+//     const liveTime = document.querySelector('.live-time');
+//     console.log(nav.offsetHeight)
+//     const totalHeight = nav.offsetHeight + timer.offsetHeight + result.offsetHeight;
+//     liveTime.style.height = `${screen.height - totalHeight}px`;
+//     console.log(liveTime.offsetHeight)
+// }
